b893e068ad007b60091ee494325cced1ec3b898d
goncalves-novo.thierry@orange.fr <THGONCAL>
2024-09-12 18:49:47 +0200

    Mise à jour du dépôt

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df=pd.read_csv("train.csv")
+
+st.title("Projet de classification binaire Titanic")
+st.sidebar.title("Sommaire")
+pages=["Exploration", "DataVizualization", "Modélisation"]
+page=st.sidebar.radio("Aller vers", pages)
+
